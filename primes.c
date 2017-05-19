f79b634865ad88a01f33c4648d2f6358b235f0a4
Alexey Reshetnyak <alexey.reshetnyak@yandex.ru>
2017-05-19 19:02:55 +0300

    Added primes.c.

---
+++ primes.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+
+int is_prime(int n)
+{
+  if (n < 4)
+    return 1;
+  if (!(n % 2))
+    return 0;
+  for (int a = 3; a < (int)(sqrt(n)) + 1; a++)
+    if (!(n % a))
+      return 0;
+  return 1;
+}
+
+int *calculate_primes(int n)
+{
+  int *primes = (int *)malloc(n * sizeof(int));
+  assert(primes != NULL);
+
+  int p = 1;
+  for (int i = 0; i < n; i++) {
+    while (!is_prime(p))
+      p++;
+    primes[i] = p;
+    p++;
+  }
+
+  return primes;
+}
+
+int main()
+{
+  int n = 1000000;
+
+  int *p = calculate_primes(n);
+
+  if (p != NULL)
+    free(p);
+
+  return 0;
+}
+
